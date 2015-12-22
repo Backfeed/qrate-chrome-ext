e469d7ce01ba9e42e438fc1d507652fd12213587
Yaniv Keinan <yanivkeinan@gmail.com>
2015-12-22 10:32:03 +0200

    upgraded node version

--- provision.sh
+++ provision.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 sudo apt-get update
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+
 sudo apt-get update -y
 sudo apt-get install -y nodejs git
+sudo apt-get install -y build-essential
 
 sudo npm install -g gulp
 sudo npm install -g bower
